ff5d9d33af0bba4727fd4402fe5f3ded79954b9e
TheSmartGerman <benegei@gmail.com>
2021-11-01 20:55:57 +0100

    Updated Script / Bugfixing

--- getMPPSolarMGX.sh
+++ getMPPSolarMGX.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 #This script assumes you already have the mpp-solar python program running correctly and node_exporter, Prometheus and Grafana working.
 # See my other documents for information on that.
@@ -17,43 +17,50 @@
 # create DISK load with # find /usr -exec grep joe {} \;
 # create Network load with # ping -f 8.8.8.8 
 
-debug=1
+# For any trouble running the script: file Encoding UTF-8 without BOM, only LF (not CRLF) - make the file excecutable 'chmod -x'
+
+debug=0
 unitName="MPP5048MGX"
-dataFileP1="MPP5048MGX_1"
-dataFileP2="MPP5048MGX_2"
-dataFileP3="MPP5048MGX_3"
+dataFileP1="MPP5048MGX"
 dataDir="/ramdisk/"
 
 while : ; do
         # Collect the raw data from inverter.
         # Use double buffering to avoid race conditions.
-        
-        #request Device General Status Parameter
-        `/home/pi/mpp-solar -p /dev/hidraw0 -c QPIGS > ${dataDir}${dataFileP1}.$$`
-        `mv ${dataDir}${dataFileP1}.$$ ${dataDir}${dataFileP1}.txt`
 
-        # request serial number
-        `/home/pi/mpp-solar -p /dev/hidraw0 -c QID > ${dataDir}${dataFileP2}.$$`
-        `mv ${dataDir}${dataFileP2}.$$ ${dataDir}${dataFileP2}.txt`
+        # sudo mpp-solar -p /dev/hidraw0 --getstatus - also possible
 
         # request Device Rating Information 
-        `/home/pi/mpp-solar -p /dev/hidraw0 -c QPIRI > ${dataDir}${dataFileP3}.$$`
-        `mv ${dataDir}${dataFileP3}.$$ ${dataDir}${dataFileP3}.txt`       
+        # RATED Information not current - apply first, otherwise you will not get actual data
+        # `mpp-solar -p /dev/hidraw0 -c QPIRI > ${dataDir}${dataFileP1}.$$`       
+        
+        #request Device General Status Parameter
+        `mpp-solar -p /dev/hidraw0 -c QPIGS >> ${dataDir}${dataFileP1}.$$`      
 
-        for mppDev in $dataFileP1 $dataFileP2 $dataFileP3
+        # request serial number
+        `mpp-solar -p /dev/hidraw0 -c QID >> ${dataDir}${dataFileP1}.$$`        
+
+        # move tmp file to txt file
+        `mv ${dataDir}${dataFileP1}.$$ ${dataDir}${dataFileP1}.txt`       
+
+        for mppDev in $dataFileP1
         do
                 # Inverter power values
                 gridVolts=`cat ${dataDir}${mppDev}.txt | \grep ac_input_voltage | cut -f2 | sed 's/ //g'`
-                batVolts=`cat  ${dataDir}${mppDev}.txt | \grep battery_voltage  | cut -f2 | sed 's/ //g'`
-                pvVolts=`cat   ${dataDir}${mppDev}.txt | \grep pv_input_voltage | cut -f2 | sed 's/ //g'`
-                pvAmps=`cat   ${dataDir}${mppDev}.txt | \grep pv_input_current_for_battery  | cut -f2 | sed 's/ //g'`
-                pvWatts=`cat   ${dataDir}${mppDev}.txt | \grep pv_input_power  | cut -f2 | sed 's/ //g'`
-                batCap=`cat    ${dataDir}${mppDev}.txt | \grep battery_capacity | cut -f2 | sed 's/ //g'`
+                gridHz=`cat  ${dataDir}${mppDev}.txt | \grep ac_input_frequency | cut -f2 | sed 's/ //g'`  
+                acOutVolt=`cat  ${dataDir}${mppDev}.txt | \grep ac_output_voltage | cut -f2 | sed 's/ //g'`  
+                acOutHz=`cat  ${dataDir}${mppDev}.txt | \grep ac_output_frequency | cut -f2 | sed 's/ //g'`  
+
                 acWatts=`cat   ${dataDir}${mppDev}.txt | \grep ac_output_active_power | cut -f2 | sed 's/ //g'`
                 acLoadPC=`cat  ${dataDir}${mppDev}.txt | \grep ac_output_load   | cut -f2 | sed 's/ //g'`
-                heatSinkTemp=`cat  ${dataDir}${mppDev}.txt | \grep inverter_heat_sink_temperature   | cut -f2 | sed 's/ //g'`
+              
+                pvVolts=`cat   ${dataDir}${mppDev}.txt | \grep pv_input_voltage | cut -f2 | sed 's/ //g'`
+                pvAmps=`cat    ${dataDir}${mppDev}.txt | \grep pv_input_current_for_battery  | cut -f2 | sed 's/ //g'`
+                pvWatts=`cat   ${dataDir}${mppDev}.txt | \grep pv_input_power  | cut -f2 | sed 's/ //g'`
+                batVolts=`cat  ${dataDir}${mppDev}.txt | \grep -w battery_voltage | cut -f2 | sed 's/ //g'` #also is_battery_voltage_to_steady_while_charging and battery_voltage_from_scc
+                batCap=`cat    ${dataDir}${mppDev}.txt | \grep battery_capacity | cut -f2 | sed 's/ //g'`
 
-                inverter_heat_sink_temperature
+                heatSinkTemp=`cat  ${dataDir}${mppDev}.txt | \grep inverter_heat_sink_temperature   | cut -f2 | sed 's/ //g'`
 
                 # We need to calculate this value
                 #pvWatts=$(echo "scale=2; $pvVolts*$pvAmps" | bc)
@@ -71,32 +78,35 @@ while : ; do
                 # Inverter Status  values (strings)
                 serNum=`cat        ${dataDir}${mppDev}.txt | \grep serial_number            | cut -f2 | sed 's/ //g'`
                 #workMode=`cat      ${dataDir}${mppDev}.txt | \grep work_mode                | cut -f2 | sed 's/ //g'`
-                srcMode=`cat       ${dataDir}${mppDev}.txt | \grep charger_source_priority  | cut -f2 | sed 's/ //g'`
+                #srcMode=`cat       ${dataDir}${mppDev}.txt | \grep charger_source_priority  | cut -f2 | sed 's/ //g'`
                 #faultCode=`cat     ${dataDir}${mppDev}.txt | \grep fault_code               | cut -f2 | sed 's/ //g'`
 
                 # create the prom file. first value should truncate file. others append!
-                printf "$mppDev{mode=\"gridVolts\"} $gridVolts\n" >  ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"batVolts\"}  $batVolts\n"  >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"pvVolts\"}   $pvVolts\n"   >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"pvAmps\"}    $pvAmps\n"    >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"batCap\"}    $batCap\n"    >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"pvWatts\"}   $pvWatts\n"   >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"acWatts\"}   $acWatts\n"   >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"acLoadPC\"}  $acLoadPC\n"  >> ${dataDir}$mppDev.prom.$$
-                printf "$mppDev{mode=\"heatSinkTemp\"}  $heatSinkTemp\n"  >> ${dataDir}$mppDev.prom.$$ 
+                printf "$mppDev{mode=\"gridVolts\"} $gridVolts\n" >  ${dataDir}${dataFileP1}.prom.$$ 
+                printf "$mppDev{mode=\"batVolts\"}  $batVolts\n"  >> ${dataDir}${dataFileP1}.prom.$$ 
+                printf "$mppDev{mode=\"pvVolts\"}   $pvVolts\n"   >> ${dataDir}${dataFileP1}.prom.$$ 
+                printf "$mppDev{mode=\"pvAmps\"}    $pvAmps\n"    >> ${dataDir}${dataFileP1}.prom.$$ 
+                printf "$mppDev{mode=\"batCap\"}    $batCap\n"    >> ${dataDir}${dataFileP1}.prom.$$ 
+                printf "$mppDev{mode=\"pvWatts\"}   $pvWatts\n"   >> ${dataDir}${dataFileP1}.prom.$$ 
+                printf "$mppDev{mode=\"acWatts\"}   $acWatts\n"   >> ${dataDir}${dataFileP1}.prom.$$ 
+                printf "$mppDev{mode=\"acLoadPC\"}  $acLoadPC\n"  >> ${dataDir}${dataFileP1}.prom.$$
+                printf "$mppDev{mode=\"gridHz\"}    $gridHz\n"    >> ${dataDir}${dataFileP1}.prom.$$
+                printf "$mppDev{mode=\"acOutVolt\"} $acOutVolt\n" >> ${dataDir}${dataFileP1}.prom.$$
+                printf "$mppDev{mode=\"acOutHz\"}    $acOutHz\n"  >> ${dataDir}${dataFileP1}.prom.$$
+                printf "$mppDev{mode=\"heatSinkTemp\"}  $heatSinkTemp\n"  >> ${dataDir}${dataFileP1}.prom.$$ 
 
                 #printf "$mppDev{mode=\"sccOK\"} $sccOK\n"             >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"sccCharging\"} $sccCharging\n" >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"acCharging\"} $acCharging\n"   >> ${dataDir}$mppDev.prom.$$ 
+                printf "$mppDev{mode=\"sccCharging\"} $sccCharging\n" >> ${dataDir}${dataFileP1}.prom.$$ 
+                printf "$mppDev{mode=\"acCharging\"} $acCharging\n"   >> ${dataDir}${dataFileP1}.prom.$$ 
                 #printf "$mppDev{mode=\"acLost\"} $acLost\n"           >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"acLoadOn\"} $acLoadOn\n"       >> ${dataDir}$mppDev.prom.$$ 
+                printf "$mppDev{mode=\"acLoadOn\"} $acLoadOn\n"       >> ${dataDir}${dataFileP1}.prom.$$ 
                 #printf "$mppDev{mode=\"batOverV\"} $batOverV\n"       >> ${dataDir}$mppDev.prom.$$ 
                 #printf "$mppDev{mode=\"batUnderV\"} $batUnderV\n"     >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"confChange\"} $confChange\n"   >> ${dataDir}$mppDev.prom.$$ 
+                printf "$mppDev{mode=\"confChange\"} $confChange\n"   >> ${dataDir}${dataFileP1}.prom.$$ 
 
-                printf "$mppDev{mode=\"serNum\"} $serNum\n"                     >> ${dataDir}$mppDev.prom.$$ 
+                printf "$mppDev{mode=\"serNum\"} $serNum\n"                     >> ${dataDir}${dataFileP1}.prom.$$ 
                 #printf "$mppDev{mode=\"workMode\",  myStr=\"$workMode\"}  0\n"  >> ${dataDir}$mppDev.prom.$$ 
-                printf "$mppDev{mode=\"srcMode\",   myStr=\"$srcMode\"}   0\n"  >> ${dataDir}$mppDev.prom.$$ 
+                #printf "$mppDev{mode=\"srcMode\",   myStr=\"$srcMode\"}   0\n"  >> ${dataDir}${dataFileP1}.prom.$$ 
                 #printf "$mppDev{mode=\"faultCode\", myStr=\"$faultCode\"} 0\n"  >> ${dataDir}$mppDev.prom.$$ 
 
                 if (( $debug )) ; then
@@ -105,8 +115,6 @@ while : ; do
         done
 
         `mv ${dataDir}${dataFileP1}.prom.$$ ${dataDir}${dataFileP1}.prom`
-        `mv ${dataDir}${dataFileP2}.prom.$$ ${dataDir}${dataFileP2}.prom`
-        `mv ${dataDir}${dataFileP3}.prom.$$ ${dataDir}${dataFileP3}.prom`
 
         sleep 4
 done
